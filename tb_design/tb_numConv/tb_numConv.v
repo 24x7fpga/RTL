726588137e8533a931dede323992fb245a0949cf
24x7fpga <kiran.24x7fpga@gmail.com>
2023-11-27 15:29:44 -0500

    test bench

---
+++ tb_design/tb_numConv/tb_numConv.v
@@ -0,0 +1,56 @@
+module tb_numConv();
+   parameter n = 4;
+ 
+/*AUTOREG*/ 
+/*AUTOWIRE*/ 
+// Beginning of automatic wires (for undeclared instantiated-module outputs)
+wire [n-1:0]		bin_2_gray;		// From DUT of numConv.v
+wire [n-1:0]		gray_2_bin;		// From DUT of numConv.v
+// End of automatics
+//reg clk; 
+//reg rst;
+   reg [n-1:0] bin;
+ 
+numConv #(.n(n)) DUT (/*AUTOINST*/
+	     // Outputs
+	     .bin_2_gray		(bin_2_gray[n-1:0]),
+	     .gray_2_bin		(gray_2_bin[n-1:0]),
+	     // Inputs
+	     .bin			(bin[n-1:0])); 
+//initial clk = 0; 
+//always #5 clk = ~clk; 
+
+
+initial begin 
+$dumpfile("tb_numConv.vcd"); 
+$dumpvars(0,tb_numConv); 
+end
+
+
+   initial begin
+      bin = 4'ha;
+      #10;
+      $finish;
+   end
+   
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("/home/kiran/fpgaProjects/iVerilog/design/numConv") 
+// End:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tb_design/tb_sm_adder/tb_sm_adder.v
@@ -0,0 +1,37 @@
+module tb_sm_adder(); 
+   parameter n = 4;
+   
+/*AUTOREG*/ 
+/*AUTOWIRE*/ 
+// Beginning of automatic wires (for undeclared instantiated-module outputs)
+wire [n-1:0]		res;			// From DUT of sm_adder.v
+// End of automatics
+reg [n-1:0] opA; 
+reg [n-1:0] opB;
+   reg [n-1:0] temp;
+   
+sm_adder #(.n(n)) DUT (/*AUTOINST*/
+	      // Outputs
+	      .res			(res[n-1:0]),
+	      // Inputs
+	      .opA			(opA[n-1:0]),
+	      .opB			(opB[n-1:0])); 
+initial begin 
+$dumpfile("tb_sm_adder.vcd"); 
+$dumpvars(0,tb_sm_adder); 
+end
+
+initial begin
+   opA = 4'b1111;
+   opB = 4'b0110;
+   #10;
+   $finish;   
+end
+  initial begin
+     temp = opB[n-2:0]-opA[n-2:0];
+  end // UNMATCHED !!
+   
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("/home/kiran/fpgaProjects/iVerilog/design/sm_adder") 
+// End:
