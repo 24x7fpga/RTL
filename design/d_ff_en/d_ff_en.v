cf0836b8b2362e28de0b4141d8d0eb2179dc219a
24x7fpga <kiran.24x7fpga@gmail.com>
2023-12-20 22:02:40 -0500

    design

---
+++ design/d_ff_en/d_ff_en.v
@@ -0,0 +1,42 @@
+`timescale 1ns/1ns 
+module d_ff_en(/*AUTOARG*/
+   // Inputs
+   d, clk, rst, en, q
+   );
+   input d;
+   input clk;
+   input rst;
+   input en;
+   output q;
+  
+   /*AUTOINPUT*/
+   /*AUTOOUTPUT*/ 
+   /*AUTOREG*/ 
+   /*AUTOWIRE*/
+    
+   reg	 d_nxt;
+   reg	 d_reg;
+
+   always@(posedge clk)
+     begin
+	if(!rst)
+	  d_reg <= 1'b0;
+	else
+	  d_reg <= d_nxt;
+     end
+
+   always@(/*AUTOSENSE*/d or d_reg or en)
+     begin
+	//d_nxt = d_reg;
+	if(en)
+	  d_nxt = d;
+	else
+	  d_nxt = d_reg;
+     end
+
+   assign q = d_reg;
+   
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/iVerilog/design/*") 
+// End:
