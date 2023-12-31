c0b2cb07bcbb29963329a684f7f8450ebe8b4bf7
24x7fpga <kiran.24x7fpga@gmail.com>
2023-12-31 14:03:18 -0500

    design

---
+++ design/fsm_pos_edge/fsm_pos_edge.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ns 
+module fsm_pos_edge(/*AUTOARG*/
+   // Outputs
+   dout_mealy, dout_moore,
+   // Inputs
+   clk, rst, din
+   );
+   input clk;
+   input rst;
+   input din;
+   output dout_mealy;
+   output dout_moore;
+   
+   /*AUTOREG*/ 
+   /*AUTOWIRE*/ 
+   // Beginning of automatic wires (for undeclared instantiated-module outputs)
+   wire			dout_moore;			// From moore of fsm_pos_edge_moore.v, ...
+   wire			dout_mealy;			// From moore of fsm_pos_edge_mealy.v, ...
+   // End of automatics
+   
+   fsm_pos_edge_moore moore (/*AUTOINST*/
+			     // Outputs
+			     .dout		(dout_moore),
+			     // Inputs
+			     .clk		(clk),
+			     .rst		(rst),
+			     .din		(din)); 
+   fsm_pos_edge_mealy mealy (/*AUTOINST*/
+			     // Outputs
+			     .dout		(dout_mealy),
+			     // Inputs
+			     .clk		(clk),
+			     .rst		(rst),
+			     .din		(din));
+ 
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/iVerilog/design/*") 
+// End:
