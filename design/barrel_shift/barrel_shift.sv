ff52dd7db57dc9d262c6a5a32e6b0c54f824fe6c
24x7fpga <kiran.24x7fpga@gmail.com>
2024-01-25 18:57:01 -0500

    barrel_shift=>update: added left shit instant to top module

--- design/barrel_shift/barrel_shift.sv
+++ design/barrel_shift/barrel_shift.sv
@@ -2,25 +2,33 @@
 `include "parameters.sv" 
 module barrel_shift(/*AUTOARG*/
    // Outputs
-   out,
+   out_rh, out_lf,
    // Inputs
-   in, rot
+   in, rt
    ); 
   input [`WIDTH-1:0] in; 
-  input [$clog2(`WIDTH)-1:0] rot;
+  input [$clog2(`WIDTH)-1:0] rt;
 
-   output [`WIDTH-1:0] 	     out;
+   output [`WIDTH-1:0] 	     out_rh;
+   output [`WIDTH-1:0] 	     out_lf;
    
  
   /*AUTOREG*/ 
   /*AUTOWIRE*/ 
  
-barrel_shift_right BSR (/*AUTOINST*/
-			// Outputs
-			.out		(out[`WIDTH-1:0]),
-			// Inputs
-			.in		(in[`WIDTH-1:0]),
-			.rot		(rot[$clog2(`WIDTH)-1:0])); 
+   barrel_shift_right BSR (/*AUTOINST*/
+			   // Outputs
+			   .out_rh		(out_rh[`WIDTH-1:0]),
+			   // Inputs
+			   .in			(in[`WIDTH-1:0]),
+			   .rt			(rt[$clog2(`WIDTH)-1:0]));
+
+   barrel_shift_left BSL (/*AUTOINST*/
+			  // Outputs
+			  .out_lf		(out_lf[`WIDTH-1:0]),
+			  // Inputs
+			  .in			(in[`WIDTH-1:0]),
+			  .rt			(rt[$clog2(`WIDTH)-1:0]));
  
 endmodule 
 // Local Variables: 
