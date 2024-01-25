ea32a55b53bbe38e3bcacd2d9037cdcbb743547c
24x7fpga <kiran.24x7fpga@gmail.com>
2024-01-25 18:58:56 -0500

    barrel_shift=>update: test bench to include right and left shift

--- tb_design/tb_barrel_shift/tb_barrel_shift.sv
+++ tb_design/tb_barrel_shift/tb_barrel_shift.sv
@@ -2,23 +2,30 @@
 `include "parameters.sv" 
 module tb_barrel_shift();
    reg [`WIDTH-1:0] in;
-   reg [$clog2(`WIDTH)-1:0] rot;
+   reg [$clog2(`WIDTH)-1:0] rt;
    
    /*AUTOREG*/ 
    /*AUTOWIRE*/ 
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
-   wire [`WIDTH-1:0]	out;			// From DUT of barrel_shift.v
+   wire [`WIDTH-1:0]	out_lf;			// From DUT of barrel_shift.v
+   wire [`WIDTH-1:0]	out_rh;			// From DUT of barrel_shift.v
    // End of automatics
    barrel_shift DUT (/*AUTOINST*/
 		     // Outputs
-		     .out		(out[`WIDTH-1:0]),
+		     .out_rh		(out_rh[`WIDTH-1:0]),
+		     .out_lf		(out_lf[`WIDTH-1:0]),
 		     // Inputs
 		     .in		(in[`WIDTH-1:0]),
-		     .rot		(rot[$clog2(`WIDTH)-1:0])); 
+		     .rt		(rt[$clog2(`WIDTH)-1:0])); 
    
-   initial in = 'h4f; 
-   initial rot = 'h1;
- 
+   initial in = 'hf04f; 
+   initial rt = 'h5;
+
+  initial begin
+     #20;  
+     $finish;
+  end
+   
    
    initial begin 
       $dumpfile("tb_barrel_shift.vcd"); 
