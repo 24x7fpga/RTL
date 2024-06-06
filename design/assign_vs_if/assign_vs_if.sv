c4c12ad267abe3d5d4db8b4212088a547ea04a77
24x7fpga <kiran.24x7fpga@gmail.com>
2024-06-06 18:28:27 -0400

    assign_vs_if: design file

---
+++ design/assign_vs_if/assign_vs_if.sv
@@ -0,0 +1,33 @@
+`timescale 1ns/1ns 
+module assign_vs_if(/*AUTOARG*/
+   // Outputs
+   d, c,
+   // Inputs
+   a, b, sel
+   ); 
+
+   // outputs
+   output logic [1:0] d;
+   output [1:0]	      c;
+   // inputs
+   input [1:0]	      a, b;
+   input	      sel;
+   
+   
+   /*AUTOREG*/ 
+   /*AUTOWIRE*/ 
+
+   // assign statement
+   assign c = sel ? a : b;
+
+   // if statement
+   always_comb
+     if(sel)
+       d = a;
+     else
+       d = b; 
+
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/iVerilog/design/*") 
+// End:
