f606ab99d5bf0a5456da9fe286bfd9089712b442
24x7fpga <kiran.24x7fpga@gmail.com>
2024-06-10 23:55:59 -0400

    encoder8_to_3: design file

---
+++ design/encoder8_to_3/encoder8_to_3.sv
@@ -0,0 +1,34 @@
+`timescale 1ns/1ns 
+module encoder8_to_3 #(parameter width = 8)(/*AUTOARG*/
+   // Outputs
+   out,
+   // Inputs
+   in
+   ); 
+   //outputs
+   output [$clog2(width)-1:0] out;
+   //inputs
+   input [width-1:0]	     in;
+   
+ 
+  /*AUTOREG*/ 
+  // Beginning of automatic regs (for this module's undeclared outputs)
+  reg [$clog2(width)-1:0] out;
+  // End of automatics
+  /*AUTOWIRE*/ 
+ 
+
+   always_comb begin
+      out = 'h0;
+      
+      for(int i = 0; i < width ; i = i+1) begin
+	 if(in[i])
+	   out = i;
+      end
+   end
+   
+ 
+endmodule 
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/iVerilog/design/*") 
+// End:
