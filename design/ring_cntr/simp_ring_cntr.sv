b9da48b3d46a877ce904a01c9f399ba0d848a1e3
24x7fpga <kiran.24x7fpga@gmail.com>
2024-07-22 13:43:02 -0400

    ring counter: design

---
+++ design/ring_cntr/simp_ring_cntr.sv
@@ -0,0 +1,30 @@
+`timescale 1ns/1ns
+module simp_ring_cntr #(parameter N = 4)(/*AUTOARG*/
+   // Outputs
+   sr_cntr,
+   // Inputs
+   clk, rst
+   );
+   // outputs
+   output [N-1:0] sr_cntr;
+   // inputs
+   input	  clk;
+   input	  rst;
+
+   /*AUTOREG*/
+   // Beginning of automatic regs (for this module's undeclared outputs)
+   reg [N-1:0]		sr_cntr;
+   // End of automatics
+   /*AUTOWIRE*/
+
+   always_ff@(posedge clk)
+     if(rst)
+       sr_cntr <= 1;
+     else
+       sr_cntr <= {sr_cntr[0],sr_cntr[N-1:1]};
+
+endmodule // simp_ring_cntr
+// Local Variables: 
+// verilog-library-directories:("~/Projects/fpgaProjects/iVerilog/design/*") 
+// End:
+   
