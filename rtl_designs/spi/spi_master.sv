cb56be9f22e01f4b6647a10802468fd6cd090cde
24x7fpga <kiran.24x7fpga@gmail.com>
2024-12-28 20:04:28 -0500

    removed unwanted files

--- rtl_designs/spi/spi_master.sv
+++
@@ -1,8 +0,0 @@
-`timescale 1ns/1ns
-module spi_master #(parameter mode = 0) (/*AUTOARG*/);
-   //outputs
-   
-endmodule // spi_master
-// Local Variables: 
-// Verilog-Library-Directories: (".")
-// End:
